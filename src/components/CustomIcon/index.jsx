d22728025abeea4e15bd598cd3e9be8118061d9c
guocong <875219399@qq.com>
2022-11-08 10:56:03 +0800

    引入antDesign组件库，完成账单列表页面的展示和筛选功能

--- src/components/CustomIcon/index.jsx
+++ src/components/CustomIcon/index.jsx
@@ -1,3 +1,3 @@
 import { Icon } from "zarm";
 
-export default Icon.createFromIconfont('//at.alicdn.com/t/c/font_3747415_z32vo2hbm1.js')
+export default Icon.createFromIconfont('//at.alicdn.com/t/c/font_3747415_sb9q5x9ptta.js')
